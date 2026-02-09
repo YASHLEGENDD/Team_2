3a1bb8aca67305b0ae491cc9081a6b6ce5bf3158
YASHLEGENDD <118902737+YASHLEGENDD@users.noreply.github.com>
2026-02-09 23:10:26 +0530

    create spring project

---
+++ employee-management-hrms/employee-management-hrms/src/test/java/com/employee/management/hrms/EmployeeManagementHrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.management.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementHrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
